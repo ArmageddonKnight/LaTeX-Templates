0c40daa3ead3d9414124cdf6cdee7e0d5071d469
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-02-04 23:42:41 -0500

    Update the templates

---
+++ upgrade.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for template in */
+do
+        cp extra_pkgs.tex ${template}
+        cd ${template}
+        make upgrade
+        git commit -am "Upgrading template" && git push
+        cd ..
+done
