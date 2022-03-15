70a59c0b2df15d3b3c55735e7b1e3d9f10ac37ac
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-03-15 17:16:22 -0400

    Upgrading template

--- upgrade.sh
+++ upgrade.sh
@@ -3,6 +3,7 @@
 for template in */
 do
         cp extra_pkgs.tex ${template}
+        cp -r docker-compose.yml dockerfiles ${template}
         cd ${template}
         make upgrade
         git commit -am "Upgrading template" && git push
