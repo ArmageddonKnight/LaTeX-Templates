a4c068464a720976b9cb32c7d1d3c3528a7da9bd
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-03-15 17:34:28 -0400

    Upgrading templates

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
 for template in ICLR IEEEtran NeurIPS USENIX acmSigConf
 do
-        cp extra_pkgs.tex ${template}
-        cp -r docker-compose.yml dockerfiles ${template}
+        printf "****************************************************************\n"
+        printf "* ${template}\n"
+        printf "****************************************************************\n"
+
+        cp -r extra_pkgs.tex docker-compose.yml dockerfiles ${template}
         cd ${template}
         make upgrade
         git add -A
